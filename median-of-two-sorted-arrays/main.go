240c506835a62c6c57e09359189b85d2f5d7b746
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-23 21:40:45 +0200

    [Median of two sorted arrays] Optimal solution.

--- median-of-two-sorted-arrays/main.go
+++ median-of-two-sorted-arrays/main.go
@@ -1,10 +1,68 @@
 package main
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func BruthForceSolution(arr1, arr2 []int) float64 {
   array := append(arr1, arr2...)
   sort.Ints(array)
   median := float64(array[(len(array) -1) >> 1] + array[len(array) >> 1]) / 2
   return median
+}
+
+func MedianOptimalApproach(arr1, arr2 []int) float64 {
+  if len(arr1) > len(arr2) {
+    arr1, arr2 = arr2, arr1
+  }
+  totalLength := len(arr1) + len(arr2)
+  half := totalLength / 2
+  for {
+    i := (len(arr1) - 1) / 2
+    j := half - i - 2
+
+    firstLeft := math.MinInt32
+    if i >= 0 {
+      firstLeft = arr1[i]
+    }
+    firstRight := math.MaxInt32
+    if i+1 < len(arr1)-1 {
+      firstRight = arr1[i+1]
+    }
+    secondLeft := math.MinInt32
+    if j >= 0 {
+      secondLeft = arr2[j]
+    }
+    secondRight := math.MaxInt32
+    if j+1 < len(arr2)-1 {
+      secondRight = arr2[j+1]
+    }
+
+    if firstLeft <= secondRight && secondLeft <= firstRight {
+      if totalLength%2 != 0 {
+        return float64(min(firstRight, secondRight))
+      } else {
+        return float64(max(firstLeft, secondLeft) + min(firstRight, secondRight)) / 2
+      }
+    } else if firstLeft > secondRight {
+      i--
+    } else {
+      j--
+    }
+  }
+}
+
+func min(a, b int) int {
+  if a < b {
+    return a
+  }
+  return b
+}
+
+func max(a, b int) int {
+  if a < b {
+    return b
+  }
+  return  a
 }
