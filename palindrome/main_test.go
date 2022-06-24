3b111312d0f7fd86cf26247055fa31720a38c7f1
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-24 07:41:22 +0200

    [Palindrome] Tests.

--- palindrome/main_test.go
+++ palindrome/main_test.go
@@ -53,3 +53,29 @@ func TestOptimalTime1(t *testing.T) {
     t.Errorf("OptimalTime1(%v) = %v\nExpected %v", input3, result3, expected3)
   }
 }
+
+func TestOptimalTime2(t *testing.T) {
+  input1 := 121
+  input2 := -121
+  input3 := 10
+
+  expected1 := true
+  expected2 := false
+  expected3 := false
+
+  result1 := OptimalTime2(input1)
+  result2 := OptimalTime2(input2)
+  result3 := OptimalTime2(input3)
+
+  if result1 != expected1 {
+    t.Errorf("OptimalTime2(%v) = %v\nExpected %v", input1, result1, expected1)
+  }
+
+  if result2 != expected2 {
+    t.Errorf("OptimalTime2(%v) = %v\nExpected %v", input2, result2, expected2)
+  }
+
+  if result3 != expected3 {
+    t.Errorf("OptimalTime2(%v) = %v\nExpected %v", input3, result3, expected3)
+  }
+}
