a93ea178b869245b06b39a47a1d270e332736b52
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-24 07:52:13 +0200

    [Palindrome] Third solution.

--- palindrome/main.go
+++ palindrome/main.go
@@ -19,3 +19,20 @@ func OptimalTime1(input int) bool {
   }
   return string(newString) == str
 }
+
+func OptimalTime2(input int) bool {
+  str := strconv.Itoa(input)
+  return helper(str, 0)
+}
+
+func helper(str string, i int) bool {
+  if i >= len(str) -1 -i {
+    return true
+  }
+
+  if str[i] != str[len(str) -1 -i] {
+    return false
+  }
+
+  return helper(str, i+1)
+}
