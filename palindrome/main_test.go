a2290a8b4550c4b7007ab0537c4f073915683439
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-24 07:36:41 +0200

    [Palindrome] Tests.

--- palindrome/main_test.go
+++ palindrome/main_test.go
@@ -27,3 +27,29 @@ func TestBruthForceSolution(t *testing.T) {
     t.Errorf("BruthForce(%v) = %v\nExpected %v", input3, result3, expected3)
   }
 }
+
+func TestOptimalTime1(t *testing.T) {
+  input1 := 121
+  input2 := -121
+  input3 := 10
+
+  expected1 := true
+  expected2 := false
+  expected3 := false
+
+  result1 := OptimalTime1(input1)
+  result2 := OptimalTime1(input2)
+  result3 := OptimalTime1(input3)
+
+  if result1 != expected1 {
+    t.Errorf("OptimalTime1(%v) = %v\nExpected %v", input1, result1, expected1)
+  }
+
+  if result2 != expected2 {
+    t.Errorf("OptimalTime1(%v) = %v\nExpected %v", input2, result2, expected2)
+  }
+
+  if result3 != expected3 {
+    t.Errorf("OptimalTime1(%v) = %v\nExpected %v", input3, result3, expected3)
+  }
+}
